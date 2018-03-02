648ce52602ebc87b75f9f418af4766f0067e15d4
lee <leewin110@126.com>
2018-03-02 15:18:36 +0800

    1.重置密码 2.更改密码 3.获取详细个人信息 4.补充

---
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -0,0 +1,34 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    int checkUserName(String username);
+
+    int checkEmail(String email);
+
+    User selectLogin(@Param("username") String username,@Param("password")String password);
+
+    String selectQuestionByUserName(String username);
+
+    int CheckAnswer(@Param("username") String username, @Param("question") String question, @Param("answer") String answer);
+
+    int updatePasswordByUserName(@Param("username") String username,@Param("password")String password);
+
+    int checkPassWord(@Param("password")String password,@Param("userId") Integer userId);
+    int checkEmailById(@Param("email")String email,@Param("userId")Integer userId);
+
+}
